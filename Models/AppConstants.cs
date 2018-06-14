6547bd32f52fb79f0bd9ac4e7ebc64eeefa1ed2a
codeprefect <mubarakadeimam@gmail.com>
2018-06-14 17:54:00 +0100

    use wwwroot for tempfiles

--- Models/AppConstants.cs
+++ Models/AppConstants.cs
@@ -8,6 +8,6 @@ namespace CommentsDownloader.Models
         public const string Amazon = "amazon";
         public const string YoutubeHost = "www.youtube.com";
         public const string AmazonHost = "www.amazon.com";
-        public static string TempFileDirectory { get => Path.Combine(Directory.GetCurrentDirectory(), "TempFolder"); }
+        public static string TempFileDirectory { get => Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "TempFolder"); }
     }
 }
